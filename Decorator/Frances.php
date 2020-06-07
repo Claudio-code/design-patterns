c008fc43d3024f762159afac8fe1f7ec4f2e5cc0
claudio-code <suporte2424@outlook.com>
2020-06-07 04:10:45 -0300

    add classe de :bread: frances

---
+++ Decorator/Frances.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "Pao.php";
+
+class Frances extends Pao
+{
+    private float $valor;
+
+    public function __construct(float $valor)
+    {
+        $this->nome = "frances";
+        $this->valor = $valor;
+    }
+
+    public function valor(): float
+    {
+        return $this->valor;
+    }
+}
