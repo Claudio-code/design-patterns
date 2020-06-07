9639efd6f51f2150d1bb7854b15890e5d9f5042a
claudio-code <suporte2424@outlook.com>
2020-06-07 04:17:03 -0300

    add classe de :bread: Baguete

---
+++ Decorator/Baguete.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "Pao.php";
+
+class Baguete extends Pao
+{
+    private float $valor;
+
+    public function __construct(float $valor)
+    {
+        $this->nome = "Baguete";
+        $this->valor = $valor;
+    }
+
+    public function valor(): float
+    {
+        return $this->valor;
+    }
+}
