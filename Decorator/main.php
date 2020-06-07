f75ce433315b7566b83d79f94d81784ebf45f967
claudio-code <suporte2424@outlook.com>
2020-06-07 04:56:37 -0300

    :heavy_check_mark: finalizado o exemplo de como implementar o decorator.

---
+++ Decorator/main.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "Frances.php";
+require_once "Salame.php";
+require_once "Baguete.php";
+
+$frances = new Frances(22);
+$baguete = new Baguete(11);
+
+$francesComSalame = new Salame($frances, 21);
+$bagueteComSalame = new Salame($baguete, 11);
+
+echo $francesComSalame->valor(). "\n";
+echo $bagueteComSalame->valor();
